071306335b47f82e2e87f9f75b257d35d7a24bb8
Amit Sahu <amitkumarsahu718@gmail.com>
2023-10-27 10:44:50 +0530

    Poll Project initiated

---
+++ poll-project/src/components/CreatePollForm.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { bind } from "svelte/internal";
+    import Button from '../shared/Button.svelte';
+
+    let fields = {question:'', answerA: '', answerB: ''};
+    let errors = {question:'', answerA: '', answerB: ''};
+    let valid = false;
+    const submitHandler = () =>{
+
+    }
+</script>
+
+
+<form on:submit|preventDefault={submitHandler}>
+    <div class="form-field">
+        <label for="question">Poll Question: </label>
+        <input type="text" id="question" bind:value={fields.question}>
+    </div>
+    <div class="form-field">
+        <label for="answer-a">Answer A: </label>
+        <input type="text" id="answer-a" bind:value={fields.answerA}>
+    </div>
+    <div class="form-field">
+        <label for="answer-b">Answer B: </label>
+        <input type="text" id="answer-b" bind:value={fields.answerB}>
+    </div>
+    <Button type="secondary" flat={true}>Add Poll</Button>
+</form>
+
+
+<style>
+    form{
+        widows: 400px;
+        margin: 0 auto;
+        text-align: center;
+    }
+    .form-field{
+        margin: 18px auto;
+    }
+    input{
+        width: 100%;
+        border-radius: 6px;
+    }
+    label{
+        margin: 10px auto;
+        text-align: left;
+    }
+
+</style>
